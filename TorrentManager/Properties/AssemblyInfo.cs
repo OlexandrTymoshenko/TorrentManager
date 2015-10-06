07b5aa4efee90464e397fc01485da425d6b7f03b
OlexandrTymoshenko <olexandr.tymoshenko@hotmail.com>
2015-10-07 00:20:22 +0300

    fixed fxcop:"Mark assemblies with NeutralResourcesLanguageAttribute"

--- TorrentManager/Properties/AssemblyInfo.cs
+++ TorrentManager/Properties/AssemblyInfo.cs
@@ -56,3 +56,5 @@ using System;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
